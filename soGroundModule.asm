a6f9465387a40f8851960c38e90df6b5569a5600
Eon <oglesby.mo@gmail.com>
2020-07-09 22:12:16 +0100

    wrote draw/[clRhombus] to draw a filled in quad

--- soGroundModule.asm
+++ soGroundModule.asm
@@ -10,80 +10,138 @@ op li r5, 0 @ $80137CF8
 op li r5, 0 @ $80137e3c
 op li r5, 0 @ $80137eec
 
-#set ECBChange to same orange as ecb drawn
-word 0xFFC040A0 @ $805A2A5C
+op b 0xEC @ $80137850
+#set ECBChange to transparent for now
+word 0x000000FF @ $805A2A5C
 
-
-
-DrawECB
-.macro drawPart(<a>, <b>)
+draw/[clRhombus2D]
+#r3 = 4 positions, x1,y1, x2, y2, x3,y3, x4,y4
+#r4 = colour
+#r5 = Zmode
+#f1 = z (probably originally line width but fuck them, this is now a full shape with no outline)
+.macro callfunc(<addr>) 
 {
+.alias temp_Hi = <addr> / 0x10000
+.alias temp_Lo = <addr> & 0xFFFF
+  lis r12, temp_Hi
+  ori r12, r12, temp_Lo
+  mtctr r12
+  bctrl	
+}
+HOOK @ $800437DC 
+{
+    stwu r1, -0x30(r1)
+    mflr r0
+    stw r0, 0x34(r1)
+    stfd f31, 0x20(r1)
+	word 0xf3e10028; //    psq_st p31, 40(r1), 0, qr0
+    fmr f31, f1
+    stw r31, 0x1C(r1)
+    mr r31, r4
+    stw r30, 0x18(r1)
+    mr r30, r5
+    stw r29, 0x14(r1)
+    mr r29, r3
+    %callfunc(0x80019FA4)
+    %callfunc(0x80018DE4)
+    lwz r31, 0(r31)
+    %callfunc(0x8001A5C0)
+    cmpwi r30, 0
+    beq zmode0
+zmode1:
+    li r3, 1
+    li r4, 3
+    li r5, 1
+    %callfunc(0x801F4774)
+    b zmodeset
+zmode0:
+    li r3, 0
+    li r4, 3
+    li r5, 1
+    %callfunc(0x801F4774)
+zmodeset:
+    li r3, 7
+    li r4, 0
+    li r5, 1
+    li r6, 7
+    li r7, 0
+    %callfunc(0x801F3FD8)
 
-	lfs f2, <a>(r3)
-	lfs f3, <a>+4(r3)
-	lfs f4, <b>(r3)
-	lfs f5, <b>+4(r3)
-	stfs f2, 0x10(r1)
-	stfs f3, 0x14(r1)
-	stfs f4, 0x18(r1)
-	stfs f5, 0x1C(r1)
+    li r3, 0x80 #triangle strip
+    li r4, 1
+    li r5, 4 #4 vertices
+    %callfunc(0x801F1088)
+    lis r3, 0xCC01 #gfx mem-loc
 
-	lfs f1, -0x68CC(r2)
-	addi r3, r1, 0x10
-	addi r4, r1, 0x08
-	li r5, 0
+.macro drawVertex(<offset>) 
+{
+	lfs f0, <offset>(r29)
+	lfs f1, <offset>+0x4(r29)
 	
-	lis r12, 0x8004
-	ori r12, r12, 0x1104
-	mtctr r12
-	bctrl
+    stfs f0, -0x8000(r3) #x
+    stfs f1, -0x8000(r3) #y
+	stfs f31, -0x8000(r3)  #z
 
-	lwz r3, 0x300(r1)
+    stw r31, -0x8000(r3) #colour
 }
-HOOK @ $8004380C
+
+	#0x0 = centre
+	#0x8 = top
+	#0x10 = bottom
+	#0x18 = left
+	#0x20 = right
+	#0x28 = bottom left bound
+	#0x30 = top right bound
+	
+	#v0
+	%drawVertex(0x08)
+	#v1
+	%drawVertex(0x18)
+	#v2
+	%drawVertex(0x10)
+	#v3
+	%drawVertex(0x20)
+
+	#draws triangle v0,v1,v2, then triangle v1,v3,v2
+
+
+	word 0xe3e10028 //    psq_l p31, 40(r1), 0, qr0
+    lwz r0, 0x34(r1)
+    lfd f31, 0x20(r1)
+    lwz r31, 0x1C(r1)
+    lwz r30, 0x18(r1)
+    lwz r29, 0x14(r1)
+    mtlr r0
+    addi r1, r1, 48
+    blr 
+}
+
+Draw ECB adjustments 
+op li r5, 1 @ $801377b8 #set zmode to true, take depth into account.
+HOOK @ $801377AC #set z-pos of previous frame to -1
 {
-	stw r3, 0x300(r1)
-	cmpwi r5, 1
-	bne prevFrame
-currentFrame:
-	lis r0, 0xFFC0
-	ori r0, r0, 0x40FF
-	b draw
-prevFrame:
-	lis r0, 0xFFC0
-	ori r0, r0, 0x4080
-draw:
-	stw r0, 0x8(r1)
-
-	%drawPart(0x8, 0x18)
-	%drawPart(0x8, 0x20)
-
-	%drawPart(0x10, 0x18)
-	%drawPart(0x10, 0x20)
+	lis r4, 0xBF80
+	stw r4, 0x64(r1)
+	lfs f1, 0x64(r1)
 }
+HOOK @ $801377C8 #set z-pos of current frame to 0
+{
+	lis r4, 0xBD00 #-0.03125 to make it not match gnw on the plane 
+	stw r4, 0x60(r1)
+	lfs f1, 0x60(r1)
+}
+word 0xFF6020FF @ $805A2A5C #previous frame colour
+word 0xFFA030FF @ $805A2A64 #current frame colour
+
 Draw Ledgegrab
-.macro drawPart(<a>, <b>, <c>, <d>)
+.macro callfunc(<addr>) 
 {
-
-	lfs f2, <a>(r1)
-	lfs f3, <b>(r1)
-	lfs f4, <c>(r1)
-	lfs f5, <d>(r1)
-	stfs f2, 0x20(r1)
-	stfs f3, 0x24(r1)
-	stfs f4, 0x28(r1)
-	stfs f5, 0x2C(r1)
-
-	lfs f1, -0x68CC(r2)
-	addi r3, r1, 0x20
-	addi r4, r1, 0x18
-	li r5, 0
-	
-	lis r12, 0x8004
-	ori r12, r12, 0x1104
-	mtctr r12
-	bctrl
-
+.alias temp_Hi = <addr> / 0x10000
+.alias temp_Lo = <addr> & 0xFFFF
+  lis r12, temp_Hi
+  ori r12, r12, temp_Lo
+  mtctr r12
+  bctrl	
 }
 
 HOOK @ $80137F44
