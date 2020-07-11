fabac6852229c97bd23fbb40f0f02ad4cb1750a8
Eon <oglesby.mo@gmail.com>
2020-07-11 13:32:22 +0100

    Draw3dline added

--- renderDebug.asm
+++ renderDebug.asm
@@ -190,3 +190,89 @@ HOOK @ $80541FB4
     addi r1, r1, 0x50
     blr 
 }
+drawLine3D [Eon]
+.macro callfunc(<addr>) 
+{
+.alias temp_Hi = <addr> / 0x10000
+.alias temp_Lo = <addr> & 0xFFFF
+  lis r12, temp_Hi
+  ori r12, r12, temp_Lo
+  mtctr r12
+  bctrl	
+}
+HOOK @ $80541fb8 
+{
+    stwu r1, -0x50(r1)
+    mflr r0
+    stw r0, 0x54(r1)
+    stfd f31, 0x20(r1)
+	word 0xf3e10030; //    psq_st p31, 0x30(r1), 0, qr0
+    fmr f31, f1
+
+
+
+    stw r31, 0x1C(r1) #pos1
+    mr r31, r3
+    stw r30, 0x18(r1) #pos2
+    mr r30, r4
+    stw r29, 0x14(r1) #colour
+	lwz r29, 0x0(r5)
+
+	stw r28, 0x10(r1) #zmode
+	mr r28, r6
+
+
+    %callfunc(0x80019FA4)
+    %callfunc(0x80018DE4)
+    %callfunc(0x8001A5C0)
+
+	#convert line width as double into line width as integer
+	lfs f0, -0x7B68(r2)
+	fmuls f0, f0, f31
+	fctiwz f0, f0
+	stfd f0, 0x8(r1)
+	lwz r3, 0xC(r1)
+	rlwinm r3, r3, 0, 24, 31
+	
+	li r4, 2
+	%callfunc(0x801f12ac)
+
+	 
+
+    li r3, 0xA8 #line
+    li r4, 1
+    li r5, 2 #2 vertices
+    %callfunc(0x801F1088)
+    lis r3, 0xCC01 #gfx mem-loc
+
+.macro drawVertex(<arg>) 
+{
+	lfs f0, 0x0(<arg>)
+	lfs f1, 0x4(<arg>)
+	lfs f2, 0x8(<arg>)
+    stfs f0, -0x8000(r3) #x
+    stfs f1, -0x8000(r3) #y
+	stfs f2, -0x8000(r3)  #z
+
+    stw r29, -0x8000(r3) #colour
+}
+	
+	#v0
+	%drawVertex(31)
+	#v1
+	%drawVertex(30)
+
+	#draws line attaching each point
+
+    lfd f31, 0x20(r1)
+	word 0xe3e10030; //    psq_l p31, 0x30(r1), 0, qr0
+
+    lwz r0, 0x54(r1)
+    lwz r31, 0x1C(r1)
+    lwz r30, 0x18(r1)
+    lwz r29, 0x14(r1)
+    lwz r28, 0x10(r1)
+    mtlr r0
+    addi r1, r1, 0x50
+    blr 
+}
