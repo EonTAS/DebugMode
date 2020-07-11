0c71aa946dbcb1f625f41eae9ca83fa45dbed45b
Eon <oglesby.mo@gmail.com>
2020-07-11 13:28:44 +0100

    tidied hitbox renderring code to pass hitbox data.
    Added commented out hitbox angle render. if i want to keep this need to have facing direction support

--- Hurtboxes Hitboxes.asm
+++ Hurtboxes Hitboxes.asm
@@ -16,7 +16,8 @@ HOOK @ $8074BDB0
 	stw r31, 0x4C(r1)
 	stw r30, 0x48(r1)
 	stw r29, 0x44(r1)
-	mr r29, r3
+	stw r28, 0x40(r1)
+	mr r28, r3
 
 #getCameraMatrix	
 	lis r12, 0x8001
@@ -51,7 +52,7 @@ HOOK @ $8074BDB0
 
 	b checkLoop
 loop:
-	lwz r3, 0x30(r29)
+	lwz r3, 0x30(r28)
 	mr r4, r30
 	lwz r12, 0(r3)
 	lwz r12, 0xC(r12)
@@ -60,15 +61,16 @@ loop:
 	lwz r0, 0(r3)
 	cmpwi r0, 0
 	beq iterate
-
+	mr r29, r3
 	mr r4, r30
 	lwz r5, 0x88(r3)
-	addi r3, r29, 0x68 
+	addi r3, r28, 0x68 
 	lis r12, 0x8074
 	ori r12, r12, 0x0A3C
 	mtctr r12
 	bctrl
 
+	mr r6, r29
 	mr r5, r3
 	mr r3, r31
 	addi r4, r1, 8
@@ -77,7 +79,7 @@ loop:
 iterate:
 	addi r30, r30, 1
 checkLoop:
-	lwz r3, 0x30(r29)
+	lwz r3, 0x30(r28)
 	lwz r12, 0(r3)
 	lwz r12, 0x14(r12)
 	mtctr r12
@@ -90,6 +92,7 @@ end:
 	lwz r31, 0x4C(r1)
 	lwz r30, 0x48(r1)
 	lwz r29, 0x44(r1)
+	lwz r28, 0x40(r1)
 	mtlr r0
 	addi r1, r1, 80
 	blr 
@@ -98,11 +101,14 @@ end:
 HOOK @ $80541fa4
 {
 debugDisplaySoCollisionAttackPart:
-	stwu r1, -0x20(r1)
+	stwu r1, -0xA0(r1)
 	mflr r0
-	stw r0, 0x24(r1)
+	stw r0, 0xA4(r1)
 	stw r31, 0x1C(r1)
 	stw r30, 0x18(r1)
+	stw r29, 0x14(r1)
+	mr r30, r5
+	mr r29, r6
 	lwz r0, 0(r3)
 	cmpwi r0, 1
 	beq skip
@@ -122,12 +128,163 @@ debugDisplaySoCollisionAttackPart:
 	lwz r12, 0x24(r12)
 	mtctr r12
 	bctrl 
+#TODO: decide if i should keep angle render
+#	lis r3, 0x4330
+#	stw r3, 0x30(r1)
+#
+#	lwz r3, 0x18(r29) #angle in degrees 
+#
+#	cmpwi r3, 361
+#	beq skip
+#	cmpwi r3, 363
+#	beq skip
+#	cmpwi r3, 365
+#	beq skip
+#
+#	stw r3, 0x34(r1)
+#	lfd f1, 0x30(r1)
+#	lfd f2, -0x6F98(r2) 
+#	fsubs f1, f1, f2 
+#	lfs f2, -0x5A6C(r2)
+#	fmuls f1, f1, f2
+#	lfs f2, -0x5B2C(r2)
+#	fdivs f1, f1, f2
+#
+#
+#	
+#	%callFunc(0x804004d8)
+#	lfd f2, 0x30(r1)
+#	stfs f1, 0x30(r1)
+#	lfd f1, -0x6F98(r2)
+#	fsubs f1, f2, f1
+#	lfs f2, -0x5A6C(r2)
+#	fmuls f1, f1, f2
+#	lfs f2, -0x5B2C(r2)
+#	fdivs f1, f1, f2
+#	%callFunc(0x804009e0)
+#
+#
+#	stfs f1, 0x34(r1)
+#
+#
+#	#do maths, put sin(r3) into 0x30(r1)
+#	#do maths, put cos(r3) into 0x34(r1)
+#	li r0, 0
+#	stw r0, 0x38(r1)
+#
+##getType
+#	mr r3, r30
+#	lwz r12, 0x30(r30)
+#	lwz r12, 0xC(r12)
+#	mtctr r12
+#	bctrl #get type
+#	cmpwi r3, 1
+#	beq capsule
+#sphere:
+#	lfs f1, 0xC(r30)
+#	lfs f2, 0x30(r1)
+#	lfs f3, 0x34(r1)
+#	fmuls f2, f1, f2
+#	fmuls f3, f1, f3
+#
+#	lfs f4, 0x0(r30)
+#	fadds f2, f2, f4
+#	lfs f5, 0x4(r30)
+#	fadds f3, f3, f5
+#	lfs f4, 0x8(r30)
+#
+#	stfs f2, 0x30(r1)
+#	stfs f3, 0x34(r1)
+#	stfs f4, 0x38(r1)
+#
+#	#pos1 = 0x0(r30)
+#	#pos2 = 0x30(r1)
+#
+#	lis r3, 0x4000
+#	stw r3, 0x3C(r1)
+#	lfs f1, 0x3C(r1)
+#
+#	mr r3, r30
+#	addi r4, r1, 0x30
+#	lis r5, 0xA0A0
+#	ori r5, r5, 0xA0FF
+#	stw r5, 0x3C(r1)
+#	addi r5, r1, 0x3C
+#	li r6, 1
+#	%callFunc(0x80541fb8)
+#	#drawLine(pos1, pos2, colour, linewidth)
+#	b skip
+#
+capsule:
+#	addi r3, r30, 0x40
+#	addi r4, r1, 0x40
+#	%callFunc(0x801ec184)
+#	
+#	addi r3, r1, 0x40
+#	addi r4, r1, 0x70
+#	%callFunc(0x801ec41c)
+#	#0x30 = transformation matrix
+#	#0x60 = inverse of transformation
+#	li r0, 0
+#	stw r0, 0x20(r1)
+#	stw r0, 0x24(r1)
+#	stw r0, 0x28(r1)
+#
+#	addi r3, r1, 0x40
+#	addi r4, r1, 0x20
+#	mr r5, r4
+#	%callFunc(0x801ecd70)
+#	addi r3, r1, 0x20
+#	addi r4, r1, 0x30
+#	mr r5, r4
+#	%callFunc(0x801ecfe4) #add angle offset to centre pos
+#
+#
+#	#at this point 0x20(r1) = center of sphere
+#	#at this point 0x30(r1) = point at angle theta away from centre unit length
+#
+#	
+#	addi r3, r1, 0x70 #inverse
+#	addi r4, r1, 0x30 #point
+#	mr r5, r4
+#	%callFunc(0x801ecd70)
+#	addi r3, r1, 0x30
+#	mr r4, r3
+#	%callFunc(0x801ed008) #normalise
+#	lfs f1, 0xC(r30)
+#
+#	lfs f2, 0x30(r1)
+#	lfs f3, 0x34(r1)
+#	lfs f4, 0x38(r1)
+#
+#	fmuls f2, f2, f1
+#	fmuls f3, f3, f1
+#	fmuls f4, f4, f1
+#
+#	stfs f2, 0x30(r1)
+#	stfs f3, 0x34(r1)
+#	stfs f4, 0x38(r1)
+#
+#	#scaled to fit radius now
+#
+#	addi r3, r1, 0x40
+#	addi r4, r1, 0x30
+#	mr r5, r4
+#	%callFunc(0x801ecd70)
+#
+#	#0x20 = pos1
+#	#0x30 = pos2
+#	#now draw a line joining the two 
+#
+#
 skip:
-	lwz r0, 0x24(r1)
+	lwz r0, 0xA4(r1)
+	
 	lwz r31, 0x1C(r1)
 	lwz r30, 0x18(r1)
+	lwz r29, 0x14(r1)
 	mtlr r0
-	addi r1, r1, 32
+	addi r1, r1, 0xA0
 	blr 
 }
 
